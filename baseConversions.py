9418a43833dd5904a6360d7b5899b5bfdac728e6
rahulnene <rahul.nene@live.com>
2018-08-01 19:32:22 +0530

    --

--- baseConversions.py
+++ baseConversions.py
@@ -2,7 +2,7 @@ from time import clock as time
 
 def numberValidInBase(input,base):
     input_string = str(input)
-    digitRep = {0:0, 1:1, 2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8 9:9, "A":10, "B":11, "C":12, "D":13, "E":14, "F":33, "G":33, "H":33, "I":33, "J":33, "K":33, "L":33, "M":33, "N":33, "O":33, "P":33, "Q":26, "R":27, "S":28, "T":29, "U":30, "V":31, "W":32, "X":33, "Y":34, "Z":35, }
+    digitRep = {0:0, 1:1, 2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8 9:9, "A":10, "B":11, "C":12, "D":13, "E":14, "F":15, "G":16, "H":17, "I":18, "J":19, "K":20, "L":21, "M":22, "N":23, "O":24, "P":25, "Q":26, "R":27, "S":28, "T":29, "U":30, "V":31, "W":32, "X":33, "Y":34, "Z":35, }
     for digit in input_string:
         if int(digit) >= base:
             print(input," cannot exist in this base (" ,base, "), try agian.")
