35b82c77d04eb7641778920e93d9f58539d1f5b5
rahulnene <rahul.nene@live.com>
2018-10-27 13:44:12 +0530

    init

--- testin1.py
+++ testin1.py
@@ -1,31 +1,9 @@
-import math
-
-abundants = []
-
-def divisorGenerator(n):
-    large_divisors = []
-    yield 1
-    for i in range(2, int(math.sqrt(n) + 1)):
-        if n % i == 0:
-            yield i
-            if i*i != n:
-                large_divisors.append(n / i)
-    for divisor in reversed(large_divisors):
-        yield divisor
-
-def abundant(k):
-    sum = 0
-    for i in list(divisorGenerator(k)):
-        sum += i
-    if sum > k: return 1
-    else: return 0
-
-def generateAbundants:
-    for n in range(24,28123):
-        if abundant(n):
-            abundants.append(n)
-
-def main:
-    generateAbundants()
-
-main()
+n = 1
+counter = 0
+for b in range(1,500):
+    for a in range(1,b+1):
+        LHS = (1/a)+ (1/b)
+        p = (LHS)*(10**n)
+        if not p%1:
+            counter += 1
+            print(a,b,p,counter)
