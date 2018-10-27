d7d107d96ed9c3a8d8b9822e12fd0c87212d028b
rahulnene <rahul.nene@live.com>
2018-10-27 09:37:52 +0530

    init

--- testin1.py
+++ testin1.py
@@ -2,12 +2,12 @@ from timeit import default_timer
 start = default_timer()
 long = 0
 dList = []
-for j in range(3,999):
+for j in range(1,999):
     if (j&2)&(j%5):dList.append(j)
 for d in dList:
     oF = 1/d
-    for i in range(1,1000):
+    for i in range(1,d):
         if not(10**i - 1)%d:
             if (i > long):long = i
             break
-print(long,"in",(default_timer()-start),"s")
+print(long,"at 1/",d,"in",(default_timer()-start)*1000,"ms")
