e32db0cda63530b7cff00965d4a3652fe2c9777a
rahulnene <rahul.nene@live.com>
2020-10-08 13:16:38 +0530

    Create RungeKutta4.py

---
+++ NumericalMethods/RungeKutta4.py
@@ -0,0 +1,34 @@
+from time import perf_counter
+from sympy import *
+
+t0 = perf_counter()
+x, y = symbols('x,y')
+
+diffy = x * sin(x)
+x0 = 0
+y0 = 1
+h = 1
+xf = 4
+
+ycurrent = y0
+xcurrent = x0
+yarr = [y0]
+
+
+def f(x0, y0):
+    return diffy.subs({x: x0, y: y0})
+
+
+while xcurrent < xf:
+    k1 = f(xcurrent, ycurrent)
+    k2 = f(xcurrent + (h / 2), ycurrent + (h * k1) / 2)
+    k3 = f(xcurrent + (h / 2), ycurrent + (h * k2) / 2)
+    k4 = f(xcurrent + h, ycurrent + h * k3)
+    ycurrent += (h / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
+    yarr.append(ycurrent)
+    xcurrent += h
+
+print(xcurrent, N(ycurrent))
+
+tf = perf_counter()
+print(tf - t0, "seconds")
